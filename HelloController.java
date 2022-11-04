62e0c6b54de4823beb1ed5e8985a8d4eb6ec0dd9
caloriecut <113892188+caloriecut@users.noreply.github.com>
2022-11-04 16:44:18 +0530

    Add files via upload

---
+++ HelloController.java
@@ -0,0 +1,15 @@
+package com.example.table;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+
+    public Button track;
+
+    public void onbuttonTrack(ActionEvent actionEvent) {
+    }
+}
